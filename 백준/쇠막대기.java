cf3d2abab2427189cc1db584b2d7f132fd75fc6b
jin7011 <fpdlqlrrja36@naver.com>
2021-12-26 17:46:32 +0900

    재활치료 21-12-27

--- 백준/쇠막대기.java
+++ 백준/쇠막대기.java
@@ -11,69 +11,11 @@ public class ¼è¸·´ë±â {
 	static Stack<Character> stk = new Stack<Character>();
 	static int sum = 0;
 
-	static public boolean isRazor(int idx) {
-
-		char next = str.charAt(idx);
-
-		if (idx != str.length() - 1) {
-			char nextnext = str.charAt(idx + 1);
-
-			if (next == '(' && nextnext == ')') {
-				stk.add('*');
-				return true;
-			}
-		}
-
-		return false;
-
-	}
-
-	static public void cal() {
-
-		int cnt = 0;
-
-		for (int x = stk.size()-1; x >= 0; x--) {
-
-			int next = stk.get(x);
-
-			if (next == '(') {
-				stk.remove(x);
-				sum += cnt + 1;
-				break;
-
-			} else if (next == '*') {
-				cnt++;
-			}
-		}
-
-
-	}
-
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		str = br.readLine();
-//
-//		for (int x = 0; x < str.length(); x++) {
-//
-//			char next = str.charAt(x);
-//
-//			if (isRazor(x)) {
-//				x++;
-//				continue;
-//			}
-//
-//			else if (next == ')') {
-//				cal();
-//			}
-//
-//			else if (next == '(') {
-//				stk.add(next);
-//			}
-//
-//		}
-//
-//		System.out.println(sum);
+
 		Stack<Integer> s = new Stack<>();
 		for (int x = 0; x < str.length(); x++) {
 			char next = str.charAt(x);
@@ -88,14 +30,7 @@ public class ¼è¸·´ë±â {
 				s.pop();
 				sum ++;
 			}
-//			if(next == '(')
-//				s.add(x);
-//			else if(s.pop() == x-1)
-//				sum += s.size();
-//			else
-//				sum++;
-//			
-			
+
 		}
 		System.out.println(sum);
 	}
