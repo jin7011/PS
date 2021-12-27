d2acbadcca32269435938a9c1c389f1ee8ab5790
jin7011 <fpdlqlrrja36@naver.com>
2021-12-27 18:26:42 +0900

    재활치료 21-12-28

--- 백준/괄호의값.java
+++ 백준/괄호의값.java
@@ -7,50 +7,57 @@ import java.util.Stack;
 
 public class 괄호의값 {
 
-	static char[] ch = new char[31];
+	static int n, ans = 0;
+	static String str = "";
 	static boolean[] visited = new boolean[31];
-	static String str;
-	static int res = 0;
 	static Stack<Character> stk = new Stack<>();
 
-	static public int dfs(int idx, int val, char ch) {
+	// (()[[]])([])
+	// 바로 나오면 일단 숫자
+	// ((
+	static int dfs(int idx, int val) {
 
-		visited[idx] = true;
-		stk.add(ch);
-
-		int sum = 0;
+		char c = str.charAt(idx);
 		char goal;
+		int sum = 0;
 		int val2 = 0;
 
-		if (ch == '(')
+		visited[idx] = true;
+		stk.add(c);
+
+		if (c == '(')
 			goal = ')';
 		else
-			goal = ']'; // ([]())
+			goal = ']';
 
 		for (int x = idx + 1; x < str.length(); x++) {
-			if (visited[x])
-				continue;
 
 			char next = str.charAt(x);
 
-			if (next == '(')
+			if (visited[x])
+				continue;
+
+			if (next == '(') {
 				val2 = 2;
-			else if (next == '[')
+//				sum += dfs(x, val2);  다음 괄호의 모양에 따라 dfs를 사용하면 올바르지 못한 괄호같은 반례를 쳐낼 수가 없다 (올바르지 못한 괄호를 지나쳐버려서)
+			} else if (next == '[') {
 				val2 = 3;
+//				sum += dfs(x, val2);
+			}
 
-			if (next != goal) {
-				sum += dfs(x, val2, next);
-			} else {
-				visited[x] = true;
+			if (next == goal) { // ( [
 				stk.pop();
-				if (sum == 0)
+				visited[x] = true;
+				if (sum == 0) {
 					return val;
-				else
+				} else {
 					return val * sum;
-
+				}
+			} 
+			else { //else문으로 올바르지 못한 괄호까지 처리할 수 있게 해주어야 stack처리가 되어서 반례에 0이 나온다
+				sum += dfs(x, val2);
 			}
 		}
-
 		return sum * val;
 	}
 
@@ -60,23 +67,21 @@ public class 괄호의값 {
 		str = br.readLine();
 
 		for (int x = 0; x < str.length(); x++) {
-
 			if (!visited[x]) {
-				int val = 0;
+				int v = 0;
 				if (str.charAt(x) == '(')
-					val = 2;
+					v = 2;
 				else if (str.charAt(x) == '[')
-					val = 3;
-
-				res += dfs(x, val, str.charAt(x));
+					v = 3;
+				
+				ans += dfs(x, v);
 			}
 		}
 
-		if (stk.size() == 0)
-			System.out.println(res);
-		else
+		if (stk.size() != 0)
 			System.out.println(0);
+		else
+			System.out.println(ans);
 
 	}
-
 }
